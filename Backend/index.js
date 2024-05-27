17dd6d78291f1523874384e8682afa06fb67f206
harshal-kitukale <harshalkitukale005@gmail.com>
2024-05-27 19:12:25 +0530

    added improved and using gemini api code convertor

---
+++ Backend/index.js
@@ -0,0 +1,98 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+require("dotenv").config(); // Load environment variables from .env file
+const cors = require("cors");
+const app = express();
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const PORT = process.env.PORT || 8000;
+
+app.use(cors()); 
+app.use(express.json());
+
+const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY);
+// Endpoint to handle code conversion
+app.get("/", (req, res) => {
+  res.json("server is healthy");
+});
+
+app.post("/convert", async (req, res) => {
+  const { code, toLanguage } = req.body;
+  if (!code || !toLanguage) {
+    return res
+      .status(400)
+      .json({ error: "Invalid request. Please provide code" });
+  }
+ 
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const prompt = `Convert this code to ${toLanguage}: ${code}`;
+    
+    const result = await model.generateContent(prompt);
+    const codeData=result.response.text();
+    
+    // const convertedCode = response.data.choices[0].text.trim();
+    return res.status(200).json({ codeData});
+  } catch (error) {
+    console.error("Error converting code:", error.message);
+
+    res.status(500).json({ error: "Failed to convert code",status:500 });
+  }
+});
+app.post("/debug", async (req, res) => {
+  const { code } = req.body;
+  console.log(code);
+  if (!code) {
+    return res
+      .status(400)
+      .json({ error: "Invalid request. Please provide code" });
+  }
+
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const prompt = `debug this code ${code}`;
+     
+    const result = await model.generateContent(prompt);
+    const codeData=result.response.text();
+    
+    // const convertedCode = response.data.choices[0].text.trim();
+    return res.status(200).json({ codeData});
+  } catch (error) {
+    console.error("Error bebugging code:", error.message);
+    if (error.response) {
+      console.error("Response data:", error.response.data);
+    }
+    res.status(500).json({ error: "Failed to debug code",status:500 });
+  }
+});
+app.post("/check", async (req, res) => {
+  const { code } = req.body;
+  console.log(code);
+  if (!code) {
+    return res
+      .status(400)
+      .json({ error: "Invalid request. Please provide code" });
+  }
+
+  try {
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro" });
+    const prompt = `quality check this code ${code}`;
+     
+    const result = await model.generateContent(prompt);
+    const codeData=result.response.text();
+    
+    // const convertedCode = response.data.choices[0].text.trim();
+    return res.status(200).json({ codeData});
+  } catch (error) {
+    console.error("Error quality checking:", error.message);
+    if (error.response) {
+      console.error("Response data:", error.response.data);
+    }
+    res.status(500).json({ error: "Failed to quality check",status:500 });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
---
+++ Frontend/scripts.js
@@ -0,0 +1,81 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const codeInput = document.getElementById("codeInput");
+  const languageSelect = document.getElementById("languageSelect");
+  const convertBtn = document.getElementById("convertBtn");
+  const debugBtn = document.getElementById("debugBtn");
+  const qualityCheckBtn = document.getElementById("qualityCheckBtn");
+  const responseDiv = document.getElementById("response");
+
+  // const BASE_URL = 'https://code-converter-api-yfl2.onrender.com';
+  const BASE_URL = "http://localhost:8080";
+  // https://backend-hkw5.onrender.com/
+
+  const handleButtonClick = async (endpoint) => {
+    const code = codeInput.value.trim();
+    const language = languageSelect.value;
+    if (!code) {
+      alert("Please enter your code.");
+      return;
+    }
+    responseDiv.style.height = "200px";
+    responseDiv.innerHTML = "Loading...";
+
+    // console.log("object", endpoint, code, language);
+    try {
+      const response = await fetch(`${BASE_URL}${endpoint}`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ code, toLanguage: language }),
+      });
+
+      const data = await response.json();
+      console.log(data.status);
+      if(data?.status==500){
+        return responseDiv.innerHTML = data.error;
+      }
+      const editedData = data?.codeData
+        ?.replace(/\*\*(.*?)\*\*/g, "<b>$1</b>")
+        .replace(/```([^```]*?)```/gs, "<pre><code>$1</code></pre>")
+        .replace(/`(.*?)`/g, "<code>$1</code>")
+        .replace(/^\* (.*)$/gm, "<li>$1</li>");
+      responseDiv.style.height = "auto";
+      let copyButton = document.createElement("button");
+      copyButton.innerText = "Copy Code";
+      copyButton.setAttribute("onclick", "copyToClipboard()");
+      copyButton.className = "copy-button";
+      // <button class="copy-button" onclick="myFunction()">Copy text</button>
+      responseDiv.innerHTML = editedData;
+      responseDiv.appendChild(copyButton);
+    } catch (error) {
+      console.error("Error:", error);
+      // responseDiv.innerHTML = "something wrong";
+    }
+  };
+
+  convertBtn.addEventListener("click", () => handleButtonClick("/convert"));
+  debugBtn.addEventListener("click", () => handleButtonClick("/debug"));
+  qualityCheckBtn.addEventListener("click", () => handleButtonClick("/check"));
+});
+
+function copyToClipboard() {
+  // Get the text content from the div
+  const textToCopy = document.getElementsByTagName("pre")[0].innerText;
+
+  // Use the Clipboard API to write text to the clipboard
+  navigator.clipboard
+    .writeText(textToCopy)
+    .then(() => {
+      console.log(textToCopy);
+      // alert('Text copied to clipboard');
+      const copyButton = document.querySelector(".copy-button");
+      copyButton.innerHTML = "Copied...";
+      setTimeout(() => {
+        copyButton.innerHTML = "Copy Code";
+      }, 1000);
+    })
+    .catch((err) => {
+      console.error("Error copying text: ", err);
+    });
+}
